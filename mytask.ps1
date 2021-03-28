5af761f514ce313890592ecda3affc22f24ef6e9
Idan malul <idanml@gmail.com>
2021-03-28 14:42:37 +0300

    Update mytask.ps1
    test policy

--- mytask.ps1
+++ mytask.ps1
@@ -1,2 +1,2 @@
 Set-Content -Path "C:\output.txt" -Force -Value "I love the bootcamp" #create (if needed) and write to the txt file
-Start-Process C:\Windows\system32\notepad.exe -filepath "c:\output.txt" #open notepad edit mode of the txt file
+Start-Process C:\Windows\system32\notepad.exe -filepath "c:\output.txt" #open notepad edit mode of the file
