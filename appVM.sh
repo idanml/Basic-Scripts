4d7d6cc39cf002079b4e81d802586a526f264507
Idan <idanml@gmail.com>
2021-04-24 15:01:50 +0300

    updated script

--- appVM.sh
+++ appVM.sh
@@ -1,8 +1,9 @@
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 apt-get update
+apt-get upgrade
 apt-get install -y nodejs
 apt-get install -y npm
 apt-get install  -y default-jdk
 cd /home/idanml/
 mkdir jenkins
 chmod -R 777 /home/idanml/jenkins/
-
