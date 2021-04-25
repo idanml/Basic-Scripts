44182ed58023f87eecc5edc485ccfdcaaf27b8eb
Idan <idanml@gmail.com>
2021-04-25 17:03:54 +0300

    add pm2

--- appVM.sh
+++ appVM.sh
@@ -4,6 +4,7 @@ apt-get upgrade
 apt-get install -y nodejs
 apt-get install -y npm
 apt-get install  -y default-jdk
+npm install pm2 -g
 cd /home/idanml/
 mkdir jenkins
 chmod -R 777 /home/idanml/jenkins/
