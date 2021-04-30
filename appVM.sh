450d60eaf4e0a5e53f00fd30ce4883a0e546ab5e
Idan <idanml@gmail.com>
2021-04-30 13:25:50 +0300

    app VM update

--- appVM.sh
+++ appVM.sh
@@ -1,6 +1,6 @@
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 apt-get update
-apt-get upgrade
+apt-get upgrade -y
 apt-get install -y nodejs
 apt-get install -y npm
 apt-get install  -y default-jdk
